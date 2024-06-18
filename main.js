8dc8691244faae2e97e710b93adbf6803a5f08bd
DGarchov <dgarchov@gmail.com>
2024-06-18 16:52:00 +0300

    Add initial movement

--- main.js
+++ main.js
@@ -13,30 +13,28 @@ const state = {
 };
 
 //Game object creation
-const gameArea=document.querySelector(".game-area");
+const gameArea = document.querySelector(".game-area");
 const factory = {
   createWizzard(wizard) {
     //create Element
-    const wizzardElement=document.createElement('div');
+    const wizzardElement = document.createElement("div");
+    wizzardElement.classList.add('wizzard')
 
     //set Styles
-    wizzardElement.style.width=wizard.width + 'px';
-    wizzardElement.style.height=wizard.height + 'px';
-    wizzardElement.style.backgroundImage='url("images/mage.png")';
-    wizzardElement.style.backgroundSize='contain';
-    wizzardElement.style.backgroundRepeat='no-repeat';
-    wizzardElement.style.backgroundPosition='center';
-    
+    wizzardElement.style.width = wizard.width + "px";
+    wizzardElement.style.height = wizard.height + "px";
+    wizzardElement.style.backgroundImage = 'url("images/mage.png")';
+    wizzardElement.style.backgroundSize = "contain";
+    wizzardElement.style.backgroundRepeat = "no-repeat";
+    wizzardElement.style.backgroundPosition = "center";
 
-   
-   
     //set position
-    wizzardElement.style.position='absolute';
-    wizzardElement.style.left=wizard.x + 'px';
-    wizzardElement.style.top=wizard.y + 'px';
+    wizzardElement.style.position = "absolute";
+    wizzardElement.style.left = wizard.x + "px";
+    wizzardElement.style.top = wizard.y + "px";
 
     //add class
-    wizzardElement.classList.add('wizzard');
+    wizzardElement.classList.add("wizzard");
     //attached to DOM
     gameArea.appendChild(wizzardElement);
   },
@@ -45,16 +43,30 @@ const factory = {
 //Input control
 
 //Game loop
+window.requestAnimationFrame(newFrame)
 
 //Game frames
-function newFrame() {}
+function newFrame() {
+ 
+  
+const wizzardElement=document.querySelector('.wizzard');
+wizzardElement.style.left= `${state.wizard.x++}px`;
 
-//Game start
+
+window.requestAnimationFrame(newFrame)
+
+}
+
 const startElement = document.querySelector(".game-start");
 startElement.addEventListener("click", (e) => {
   //Hide start element
   e.currentTarget.classList.add("hidden");
 
+  
+  
   //Initialize game
   factory.createWizzard(state.wizard);
+
+  //Game start
+  window.requestAnimationFrame(newFrame)
 });
